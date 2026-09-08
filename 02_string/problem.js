b2f3bc943bf2938e429796c6d9327cd152a80058
Prathamesh-coder-01 <prathameshdabhade71@gmail.com>
2026-09-08 11:36:48 +0530

    learn String intro,methods

---
+++ 02_string/problem.js
@@ -0,0 +1,70 @@
+// let str = "hello";
+
+// let rev =str.split("").reverse().join("");
+// console.log (rev)
+
+// let rev = "";
+// for (let i = str.length - 1; i >= 0; i--) {
+//   rev += str[i];
+// }
+// console.log(rev);
+
+// let str = "javascript";
+// let count = 0;
+// let vowels = "aeiou";
+// for (let ch of str) {
+//   if (vowels.includes(ch)) {
+//     count++;
+//   }
+// }
+// console.log(count);
+
+// let str = "Hello I am A Jvascript Learner";
+// let count = 0;
+// let vowels = "AEIOUaeiou";
+
+// for (let char of str) {
+//   if (vowels.includes(char)) {
+//     count++;
+//   }
+// }
+// console.log(count);
+
+// let str = "banana";
+// let count = str.split("a").length - 1;
+// console.log(count);
+// console.log(str);
+// console.log(str.length);
+
+// let str = "javascript";
+
+// let newStr = str.includes("script");
+// console.log(newStr);
+
+// let str = "progamming";
+// let result = [...new Set(str)].join("");
+// console.log(result);
+
+// let str = "programming";
+
+// let result = [...new Set(str)].join("");
+
+// console.log(result);
+
+// let str = "hello";
+// // let capa = str.toUpperCase();
+// // console.log(capa);
+// let result = str.slice(0, 2) + str.charAt(2).toUpperCase() + str.slice(3);
+// console.log(result);
+
+// let str = "Hello";
+
+// console.log(str.split(""));
+
+// let str = "hello";
+// console.log(str.split("").reverse().join(""));
+
+// let str = "I Love Javascript language";
+// console.log(str.split(" ").length);
+
+
---
+++ 02_string/script.js
@@ -0,0 +1,123 @@
+// let str = "ecoecaec";
+
+// let count = 0;
+// for (let i = 0; i <= str.length; i++) {
+//   if (str.startsWith("ec", i)) {
+//     count++;
+//   }
+// }
+// console.log(count);
+
+// endswith statment example
+// let str = "Check";
+// console.log (str.startsWith("Ch"));
+// console.log (str.startsWith("Ce"));
+// console.log (str.endsWith("ck"));
+// console.log (str.endsWith("ckl"));
+
+// console.log ("name");
+
+// let str = "image.info.pdf";
+// console.log(str.startsWith(".pdf"));
+// console.log(str.endsWith(".pdf"));
+
+// let str = "Check";
+// let length = str.length;
+// console.log(length);
+// console.log(str.startsWith("Check", 3));
+
+// console.log(str.slice(3));
+// console.log (str.slice(-2))
+
+// let str = "validate";
+// for (let i = 0; i < str.length - 1; i++) {
+//   console.log(str.slice(i, i + 2));
+// }
+// let str = "abhishek";
+
+// console.log(str.toLocaleUpperCase(str));
+// console.log(str.slice(0, 1).toLocaleUpperCase() + str.slice(1));
+
+// let str = "hello";
+// str[0] = "Y";
+// console.log(str);
+// let arr = [1, 2, 3];
+// arr[0] = 9;
+// console.log(arr);
+
+// let str = "madam";
+// let rev = "";
+
+// for (let i = str.length - 1; i >= 0; i--) {
+//   rev += str[i];
+// }
+// console.log(rev);
+
+// if (str === rev) {
+//   console.log("Is Palindrome string");
+// } else {
+//   console.log("Is Not a palindrome string");
+// }
+
+// let str = "Hello";
+// let repl = str.replace("H", "L");
+// console.log(repl);
+// console.log(str);
+
+// let str = "hello world  hello";
+
+// console.log ("hello", "world")
+
+// console.log (str.replaceAll("h", "w"))
+
+// let str = "hello world  hello";
+// let res = [];
+// for (let i = 0; i <= str.length - 1; i++) {
+//   res += str[i];
+// }
+// console.log(res);
+
+// let rev = str.split("").join("");
+// console.log(rev);
+
+// let str = " hello my name is Prathamesh";
+
+// let count = str.split(" ").length;
+// console.log(count);
+
+// let name = "Prathamesh";
+// console.log(` My name is ${name} !`);
+
+// let str = "Hello";
+
+// console.log (str[0]);
+// console.log (str[2]);
+// console.log (str[1]);
+
+// let str = "Hello";
+
+// console.log(str.charAt(4));
+// console.log(str[4]);
+
+// let str ="javascript";
+
+// console.log (str.slice (2,-5))
+// console.log (str.substring(0,5))
+
+// console.log (str.includes("java"));
+
+// console.log (str.search("ript"))
+
+// let str = "  ball cat";
+// let str2 = "mango apple";
+// console.log (str.replace("ball", "cat"));
+// console.log (str.replaceAll("ball", "cat").toUpperCase([]));
+
+// console.log(str.split(","));
+
+// console.log(str.concat(" ", str2));
+// console.log (str.repeat(3))
+// let str = "hello"
+// console.log (str.localeCompare());
+
+
