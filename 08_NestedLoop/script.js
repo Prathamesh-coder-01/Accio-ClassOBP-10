a858f10f6a7b201bd8979507f32e8825dc86e462
Prathamesh-coder-01 <prathameshdabhade71@gmail.com>
2026-09-22 12:28:14 +0530

    Learn The concepts of Loops and Nested Loops

---
+++ 08_NestedLoop/script.js
@@ -0,0 +1,33 @@
+// for (let i = 1; i <= 3; i++) {
+//   for (let j = 1; j <= 2; j++) {
+//     console.log(i);
+//   }
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//   let row = "";
+
+//   for (let j = 1; j <= i; j++) {
+//     row += "* ";
+//   }
+
+//   console.log(row);
+// }
+
+// for (let i = 1; i <= 3; i++) {
+//   for (let j = 1; j <= 10; j++) {
+//     console.log(i + " x " + j + " = " + i * j);
+//   }
+// }
+
+// let num = 5;
+
+for (let i = 0; i < 5; i++) {
+  let row = "";
+
+  for (let j = 0; j < i; j++) {
+    row += "* ";
+  }
+}
+// console.log(row);
+console.log(row);
